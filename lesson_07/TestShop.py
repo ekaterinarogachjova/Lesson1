f25dafce7592bba67ebd5150f9dbc9c0238d415f
ekaterinarogachjova <ekaterina.rogachjova@gmail.com>
2025-04-21 20:38:44 +0300

    Седьмой коммит ver2

--- lesson_07/TestShop.py
+++ lesson_07/TestShop.py
@@ -1,3 +1,4 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
@@ -6,15 +7,20 @@ from CartPage import CartPage
 from CheckoutPage import CheckoutPage
 from LoginPage import LoginPage
 from MainPage import MainPage
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
-def test_sauce_demo():
-    options = Options()
-    options.add_argument("--blink-settings=imagesEnabled=false")
-    service = ChromeService(executable_path=ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
+class TestSauceDemo(unittest.TestCase):
 
-    try:
+    def setUp(self):
+        options = Options()
+        options.add_argument("--blink-settings=imagesEnabled=false")
+        service = ChromeService(executable_path=ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=service, options=options)
+
+    def test_sauce_demo(self):
+        driver = self.driver
         driver.get("https://www.saucedemo.com/")
 
         # Инициализация страниц
@@ -28,9 +34,13 @@ def test_sauce_demo():
         login_page.enter_password("secret_sauce")
         login_page.click_login()
 
+        # Ожидание алерта
+        #WebDriverWait(driver, 20).until(EC.alert_is_present())
+        #alert = driver.switch_to.alert
+        #alert.accept()
+
         # Добавление товаров в корзину
-        items_to_add = ["Sauce Labs Backpack",
-                        "Sauce Labs Bolt T-Shirt", "Sauce Labs Onesie"]
+        items_to_add = ["Sauce Labs Backpack", "Sauce Labs Bolt T-Shirt", "Sauce Labs Onesie"]
         for item in items_to_add:
             main_page.add_item_to_cart(item)
 
@@ -44,12 +54,11 @@ def test_sauce_demo():
 
         # Проверка итоговой стоимости
         total_cost_value = checkout_page.get_total_cost()
-        assert total_cost_value == 58.29, f"Итоговая сумма должна быть 58.29, но получено {total_cost_value}"  # noqa: E501
+        self.assertEqual(total_cost_value, 58.29, f"Итоговая сумма должна быть 58.29, но получено {total_cost_value}")
 
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    finally:
-        driver.quit()
+    def tearDown(self):
+        self.driver.quit()
 
 
-test_sauce_demo()
+if __name__ == "__main__":
+    unittest.main()
